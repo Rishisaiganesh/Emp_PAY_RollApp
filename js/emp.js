75bbe756c46ef1093bde95902d9df17aa17c3750
Rishisaiganesh <crazyganesh67@gmail.com>
2021-07-19 21:27:11 +0530

    Validating DateandName Setter

--- js/emp.js
+++ js/emp.js
@@ -1,4 +1,5 @@
-
+window.addEventListener('DOMContentLoaded',(event)=>{
+    
 //creating method EventListener to salary
 var salary = document.querySelector('#salary')
 var output = document.querySelector('.salary-output')
@@ -6,63 +7,49 @@ output.textContent = salary.value
 salary.addEventListener('input',function(){
     output.textContent= salary.value;
 })
-
-class EmployeePayrollData {
-    get id() {
-        return this.id;
-    }
-    
-    get name() {
-        return this.name;
-    }
-    set name(name) {
-        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{3}$');
-        if(nameRegex.test(name))
-        this.name = name;
-        else
-        throw 'Invalid Name content';
-    }
-    get profilePic() {
-        return this.profilePic;
-    }
-    set profilePic(profilePic){
-        this.profilePic = profilePic;
-    }
-    get gender() {
-        return this.gender;
-    }
-    set gender(gender) {
-        this.gender = gender;
-    }
-    get department(){
-        return this.department;
-    }
-    set department(department) {
-        this.department = department;
-    }
-    get salary() {
-        return this.salary
-    }
-    set salary(salary) {
-        this.salary = salary;
-    }
-    get note() {
-        return this.note;
-    }
-    set note(note) {
-        this.note = note;
-    }
-    get startDate() {
-        return this.startDate;
-    }
-    set startDate(startDate) {
-        this.startDate = startDate;
-    }
-    toString() {
-    
-    const option = {year: 'number', month:'long', day='number'};
-    const empDate = !this.startdate ? 'undefind': this.startdate.toLocalDataString('en-US',option);
-    return "id=" +this.id+ ',name=' + this.name + ',salary=' +this.salary+ ',gender=' + this.gender+ 'startDate=' + empDate;
-}
-}
+ var name = document.querySelector("#name")
+ var submitButton = document.querySelector("#submitbutton")
+ var textError = document.querySelector(".text-error")
+ let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2}$')
+ name.addEventListener('input',function(){
+        if(nameRegex.test(name.value))
+        {
+            textError.textContent ='valid name'
+            submitButton.disabled = false;
+        }
+        if(!nameRegex.test(name.value))
+        {
+            textError.textContent='name invalid'
+            submitButton.disabled= true
+        }
+ })
+    var day = document.querySelector("#day")
+    var month = document.querySelector('#month')
+    var year = document.querySelector('#year');
+    var currDate = new Date()
+    function setCurrentDate() {
+        month.value = currDate.getMonth();
+        day.value = currDate.getDate();
+        year.value = currDate.getFullYear();
+      }
+      function validateDate() {
+        // future dates
+        if (year.value == currDate.getFullYear() && month.value <= currDate.getMonth() &&
+         day.value <= currDate.getDate()) 
+        {
+          textError.textContent = "";
+      
+        } else if (year.value < currDate.getFullYear()) {
+          textError.textContent = "";
+        } else if (year.value > currDate.getFullYear()) {
+          setCurrentDate();
+        } else  (year.value == currDate.getFullYear() && month.value > currDate.getMonth()) 
+        {
+          setCurrDate();
+        } 
+      }
+      year.addEventListener("input", validateDate);
+      month.addEventListener("input", validateDate);
+      day.addEventListener("input", validateDate);    
+});
 
